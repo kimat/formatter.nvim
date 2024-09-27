886948e318c340bce36031d89dd546c8ca38d8a3
Kimat Boven <kimat@users.noreply.github.com>
2024-09-27 18:34:34 +0200

    wip

--- lua/formatter/filetypes/ruby.lua
+++ lua/formatter/filetypes/ruby.lua
@@ -22,9 +22,6 @@ function M.standardrb()
     exe = "standardrb",
     args = {
       "--fix",
-      "--format",
-      "quiet",
-      "--stderr",
       "--stdin",
       util.escape_path(util.get_current_buffer_file_path()),
     },
